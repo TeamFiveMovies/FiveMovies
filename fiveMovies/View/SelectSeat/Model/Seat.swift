4afb793f6e6dc1ae647e28e914d65cac7d398086
JosephSeong <joseph0903@naver.com>
2024-01-16 14:56:05 +0900

    Add selectSeat UI

---
+++ fiveMovies/View/SelectSeat/Model/Seat.swift
@@ -0,0 +1,13 @@
+//
+//  Seat.swift
+//  fiveMovies
+//
+//  Created by Joseph on 1/16/24.
+//
+
+import Foundation
+
+struct Seat {
+    var isAvailable: Bool   // 좌석이 사용 가능한지
+    var isSelected: Bool    // 좌석이 선택되었는지
+}
--- fiveMovies/View/SelectSeat/SelectSeatViewController.swift
+++ fiveMovies/View/SelectSeat/SelectSeatViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class SelectSeatViewController: UIViewController {
 
+    var seats: [Seat] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        // 좌석 정보 초기화
+        seats = [
+            Seat(isAvailable: true, isSelected: false),
+            Seat(isAvailable: false, isSelected: false)
+        ]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // 좌석에 대한 초기 UI 설정
+        updateSeatUI()
     }
-    */
+
+    @IBAction func seatTapped(_ sender: UIButton) {
+        let seatIndex = sender.tag
+        let selectedSeat = seats[seatIndex]
+
+        if selectedSeat.isAvailable {
+            seats[seatIndex].isSelected.toggle()
+            updateSeatUI()
+        } else {
+            // 선택 불가능한 좌석에 대한 처리
+        }
+
+        print(#function)
+    }
+
+    func updateSeatUI() {
+//        for (index, seat) in seats.enumerated() {
+//            let button = view.viewWithTag(index) as? UIButton
+//
+//        }
+    }
 
 }

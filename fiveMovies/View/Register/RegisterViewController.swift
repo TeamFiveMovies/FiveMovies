9dd4bc8e5b2b091f81615bea05c12f894d1baa61
mysubmin <gnasus2884@naver.com>
2024-01-17 10:20:35 +0900

    유저 회원가입 var1

--- fiveMovies/View/Register/RegisterViewController.swift
+++ fiveMovies/View/Register/RegisterViewController.swift
@@ -14,25 +14,31 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordCheck: UITextField!
     @IBOutlet weak var birthDate: UIDatePicker!
     
-    var userData = UserData()
+    var userDataList: [UserData] = []
+    let userDataKey = "User"
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        userDataList = readUserList()
+        print("\(userDataList)")
     }
     
     @IBAction func confirmBtnTap(_ sender: UIButton) {
         
         //UserDefaults에 회원정보 저장
-        let newUser = UserData.UserDatas(id: registerID.text!, password: registerPassword.text!, birth: birthDate.date)
+        let newUser = UserData(id: registerID.text!, password: registerPassword.text!, birth: birthDate.date)
         
         
-        if userData.dateChecking(userData: newUser) {
+        if dateChecking(userData: newUser) {
             
             let alert = UIAlertController(title: "회원가입 가능합니다", message: "설명", preferredStyle: .alert)
             let addAction = UIAlertAction(title: "가입하기", style: .default){_ in
                 
-                self.userData.addUserList(userData: newUser)
+                self.addUserList(userInfo: newUser)
+                print("\(self.userDataList)")
+                print("\(self.readUserList())")
                 self.dismiss(animated: true)
             }
 
@@ -55,5 +61,50 @@ class RegisterViewController: UIViewController {
 }
 
 extension RegisterViewController {
+    // 유저 정보 추가
+    func addUserList(userInfo: UserData) {
+        userDataList.append(userInfo)
+        
+        print("\(userDataList)")
+        
+        guard let encodedData = try? JSONEncoder().encode(userInfo) else { return }
+        UserDefaults.standard.set(encodedData, forKey: userDataKey)
+
+        
+    }
     
+    // 전체 유저 정보 조회
+    func readUserList() -> [UserData]{
+       guard let userListData = UserDefaults.standard.data(forKey: userDataKey),
+             let decodedData = try? JSONDecoder().decode([UserData].self, from: userListData)
+       else {
+           print("왜들어갈까?")
+           return [] }
+        
+        userDataList = decodedData
+        
+        return decodedData
+    }
+    
+
+    
+    func dateChecking(userData: UserData) -> Bool {
+        // 아이디, 비밀번호 빈칸 체크
+        if userData.id  == ""{
+            
+            return false
+        }
+        if userData.password == "" {
+            return false
+        }
+        
+        // 아이디 중복 체크
+        for user in userDataList {
+            if user.id == userData.id{
+                return false
+            }
+        }
+        
+        return true
+    }
 }
--- fiveMovies/View/Register/UserData.swift
+++ fiveMovies/View/Register/UserData.swift
@@ -8,59 +8,10 @@
 import Foundation
 
 
-class UserData {
-    struct UserDatas: Codable {
-        var id: String
-        var password: String
-        var birth: Date
-    }
-    
-    var userDataList: [UserDatas] = []
-    let userDataKey = "User"
-    
-    
-    func dateChecking(userData: UserDatas) -> Bool {
-        // 아이디, 비밀번호 빈칸 체크
-        if userData.id  == ""{
-            
-            return false
-        }
-        if userData.password == "" {
-            return false
-        }
-        
-        // 아이디 중복 체크
-        for user in readUserList() {
-            if user.id == userData.id{
-                return false
-            }
-        }
-        
-        return true
-        
-    }
-    
-    // 유저 정보 추가
-    func addUserList(userData: UserDatas) {
-        userDataList = readUserList()
-        userDataList.append(userData)
-        
-        guard let encodedData = try? JSONEncoder().encode(userData) else { return }
-        UserDefaults.standard.set(encodedData, forKey: userDataKey)
-        print("\(encodedData) 추가")
-        print("\(userDataList)")
-        print("\(readUserList())")
-        
-    }
-    
-    // 전체 유저 정보 조회
-    func readUserList() -> [UserDatas]{
-        guard let userListData = UserDefaults.standard.data(forKey: userDataKey),
-              let decodedData = try? JSONDecoder().decode([UserDatas].self, from: userListData) else { return [] }
-        
-        userDataList = decodedData
-        
-        return decodedData
-    }
+
+struct UserData: Codable {
+    var id: String
+    var password: String
+    var birth: Date
 }
 
